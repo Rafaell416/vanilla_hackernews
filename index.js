49a5fe0e501e98ed5e01a2e176f991f41aeb38fd
Rafel Villarreal <rvillarreal416@gmail.com>
2018-08-23 19:26:40 -0500

    process port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT
 
 app.use(express.static('public'))
 
@@ -10,7 +10,7 @@ app.get('/', (req, res) => {
 
 app.listen(port, (err) => {
   if (err) {console.log(err)}
-  console.log('server is runnig ok in port' + port)
+  console.log('server is runnig ok in port ' + port)
 })
 
 module.exports = app
